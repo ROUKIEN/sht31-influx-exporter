9e464e6e14d0919b796e1a531b4a0ebfb4190c80
Rukien <julien.jean@froupit.org>
2021-02-16 22:03:23 +0100

    decent repo structure

--- devices/sht31/sht31_test.go
+++ devices/sht31/sht31_test.go
@@ -1,1 +1,25 @@
 package sht31
+
+import (
+	"testing"
+)
+
+func TestToTemperatureCelsius(t *testing.T) {
+	bytes := []byte{96, 37, 167, 153, 152, 143}
+
+	result := ToTemperatureCelsius(bytes)
+	const expected = 20.724800
+	if result != expected {
+		t.Errorf("It failed. Expected %f, got %f", expected, result)
+	}
+}
+
+func TestToRelativeHumidity(t *testing.T) {
+	bytes := []byte{96, 37, 167, 153, 152, 143}
+
+	result := ToRelativeHumidity(bytes)
+	const expected = 59.998474
+	if result != expected {
+		t.Errorf("It failed. Expected %f, got %f", expected, result)
+	}
+}
